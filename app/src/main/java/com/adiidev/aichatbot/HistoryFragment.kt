71acc620e9c3a41da2d64cc171dc901d9ffbad9c
aditygarg732-ctrl <aditygarg732@gmail.com>
2026-05-23 17:24:43 +0530

    2nd commit

---
+++ app/src/main/java/com/adiidev/aichatbot/HistoryFragment.kt
@@ -0,0 +1,5 @@
+package com.adiidev.aichatbot
+
+import androidx.fragment.app.Fragment
+
+    class HistoryFragment : Fragment(R.layout.fragment_history)
--- app/src/main/java/com/adiidev/aichatbot/MainActivity.kt
+++ app/src/main/java/com/adiidev/aichatbot/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.adiidev.aichatbot
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,6 +20,40 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val menuBtn = findViewById<ImageButton>(R.id.menuButton)
+        val newChatBtn = findViewById<ImageButton>(R.id.newChatButton)
+        val sendBtn = findViewById<ImageButton>(R.id.sendBtn)
+        val message_Ai = findViewById<EditText>(R.id.message_edittext)
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val historyOption = findViewById<ImageButton>(R.id.historyOption)
+        val settingOption = findViewById<ImageButton>(R.id.settingsOption)
+
+        menuBtn.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+            supportFragmentManager.beginTransaction().replace(R.id.drawerFragmentContainer ,
+                HistoryFragment()).commit()
+        }
+        newChatBtn.setOnClickListener {
+            Toast.makeText(this, "newChat Button clicked!", Toast.LENGTH_SHORT).show()
+        }
+        sendBtn.setOnClickListener {
+            val message = message_Ai.text.toString()
+            if (message.isNotEmpty()){
+                Toast.makeText(this, "Menu Button clicked!", Toast.LENGTH_SHORT).show()
+            }
+            message_Ai.text.clear()
+        }
+        historyOption.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.drawerFragmentContainer ,
+                HistoryFragment()
+            ).commit()
+        }
+        settingOption.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.drawerFragmentContainer ,
+                SettingsFragment()
+            ).commit()
         }
     }
 }
