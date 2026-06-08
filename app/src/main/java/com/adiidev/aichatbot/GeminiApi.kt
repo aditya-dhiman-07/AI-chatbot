69a98a4ba974976ef88044198eaa99c12eb40d12
aditygarg732-ctrl <aditygarg732@gmail.com>
2026-06-08 16:27:36 +0530

    5th commit

--- app/src/main/java/com/adiidev/aichatbot/GeminiApi.kt
+++ app/src/main/java/com/adiidev/aichatbot/GeminiApi.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 
 interface GeminiApi {
 
-    @POST("v1beta/models/gemini-1.5-flash:generateContent")
+    @POST("v1beta/models/gemini-2.5-flash:generateContent")
     suspend fun generateContent(
         @Query("key") apiKey: String,
         @Body body: GeminiRequest
